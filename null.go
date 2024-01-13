ac331314a09c1849e1a1e8a37afeced02ca72f8a
Stepan Bukhtii <stepanbukhtii@gmail.com>
2024-01-13 02:16:46 +0200

    add null valid function

--- null.go
+++ null.go
@@ -13,11 +13,21 @@ func NullString(s string) sql.NullString {
 	}
 }
 
+func NullStringValid(s string) sql.NullString {
+	return sql.NullString{
+		String: s,
+		Valid:  true,
+	}
+}
+
 func NullStringPtr(s *string) sql.NullString {
 	if s == nil {
-		return sql.NullString{String: "", Valid: false}
+		return sql.NullString{Valid: false}
 	}
-	return sql.NullString{String: *s, Valid: true}
+	return sql.NullString{
+		String: *s,
+		Valid:  true,
+	}
 }
 
 func StringPtr(s sql.NullString) *string {
@@ -28,14 +38,27 @@ func StringPtr(s sql.NullString) *string {
 }
 
 func NullInt64(i int64) sql.NullInt64 {
-	return sql.NullInt64{Int64: i, Valid: i != 0}
+	return sql.NullInt64{
+		Int64: i,
+		Valid: i != 0,
+	}
+}
+
+func NullInt64Valid(i int64) sql.NullInt64 {
+	return sql.NullInt64{
+		Int64: i,
+		Valid: true,
+	}
 }
 
 func NullInt64Ptr(i *int64) sql.NullInt64 {
 	if i == nil {
-		return sql.NullInt64{Int64: 0, Valid: false}
+		return sql.NullInt64{Valid: false}
 	}
-	return sql.NullInt64{Int64: *i, Valid: true}
+	return sql.NullInt64{
+		Int64: *i,
+		Valid: true,
+	}
 }
 
 func Int64Ptr(i sql.NullInt64) *int64 {
@@ -46,14 +69,27 @@ func Int64Ptr(i sql.NullInt64) *int64 {
 }
 
 func NullInt32(i int32) sql.NullInt32 {
-	return sql.NullInt32{Int32: i, Valid: i != 0}
+	return sql.NullInt32{
+		Int32: i,
+		Valid: i != 0,
+	}
+}
+
+func NullInt32Valid(i int32) sql.NullInt32 {
+	return sql.NullInt32{
+		Int32: i,
+		Valid: true,
+	}
 }
 
 func NullInt32Ptr(i *int32) sql.NullInt32 {
 	if i == nil {
-		return sql.NullInt32{Int32: 0, Valid: false}
+		return sql.NullInt32{Valid: false}
 	}
-	return sql.NullInt32{Int32: *i, Valid: true}
+	return sql.NullInt32{
+		Int32: *i,
+		Valid: true,
+	}
 }
 
 func Int32Ptr(i sql.NullInt32) *int32 {
@@ -64,14 +100,27 @@ func Int32Ptr(i sql.NullInt32) *int32 {
 }
 
 func NullInt16(i int16) sql.NullInt16 {
-	return sql.NullInt16{Int16: i, Valid: i != 0}
+	return sql.NullInt16{
+		Int16: i,
+		Valid: i != 0,
+	}
+}
+
+func NullInt16Valid(i int16) sql.NullInt16 {
+	return sql.NullInt16{
+		Int16: i,
+		Valid: true,
+	}
 }
 
 func NullInt16Ptr(i *int16) sql.NullInt16 {
 	if i == nil {
-		return sql.NullInt16{Int16: 0, Valid: false}
+		return sql.NullInt16{Valid: false}
 	}
-	return sql.NullInt16{Int16: *i, Valid: true}
+	return sql.NullInt16{
+		Int16: *i,
+		Valid: true,
+	}
 }
 
 func Int16Ptr(i sql.NullInt16) *int16 {
@@ -82,14 +131,27 @@ func Int16Ptr(i sql.NullInt16) *int16 {
 }
 
 func NullByte(b byte) sql.NullByte {
-	return sql.NullByte{Byte: b, Valid: b != 0}
+	return sql.NullByte{
+		Byte:  b,
+		Valid: b != 0,
+	}
+}
+
+func NullByteValid(b byte) sql.NullByte {
+	return sql.NullByte{
+		Byte:  b,
+		Valid: true,
+	}
 }
 
 func NullBytePtr(b *byte) sql.NullByte {
 	if b == nil {
-		return sql.NullByte{Byte: 0, Valid: false}
+		return sql.NullByte{Valid: false}
 	}
-	return sql.NullByte{Byte: *b, Valid: true}
+	return sql.NullByte{
+		Byte:  *b,
+		Valid: true,
+	}
 }
 
 func BytePtr(i sql.NullByte) *byte {
@@ -100,14 +162,27 @@ func BytePtr(i sql.NullByte) *byte {
 }
 
 func NullFloat64(f float64) sql.NullFloat64 {
-	return sql.NullFloat64{Float64: f, Valid: f != 0}
+	return sql.NullFloat64{
+		Float64: f,
+		Valid:   f != 0,
+	}
+}
+
+func NullFloat64Valid(f float64) sql.NullFloat64 {
+	return sql.NullFloat64{
+		Float64: f,
+		Valid:   true,
+	}
 }
 
 func NullFloat64Ptr(f *float64) sql.NullFloat64 {
 	if f == nil {
-		return sql.NullFloat64{Float64: 0, Valid: false}
+		return sql.NullFloat64{Valid: false}
 	}
-	return sql.NullFloat64{Float64: *f, Valid: true}
+	return sql.NullFloat64{
+		Float64: *f,
+		Valid:   true,
+	}
 }
 
 func Float64Ptr(f sql.NullFloat64) *float64 {
@@ -118,14 +193,27 @@ func Float64Ptr(f sql.NullFloat64) *float64 {
 }
 
 func NullDecimal(d decimal.Decimal) decimal.NullDecimal {
-	return decimal.NullDecimal{Decimal: d, Valid: !d.IsZero()}
+	return decimal.NullDecimal{
+		Decimal: d,
+		Valid:   !d.IsZero(),
+	}
+}
+
+func NullDecimalValid(d decimal.Decimal) decimal.NullDecimal {
+	return decimal.NullDecimal{
+		Decimal: d,
+		Valid:   true,
+	}
 }
 
 func NullDecimalPtr(d *decimal.Decimal) decimal.NullDecimal {
 	if d == nil {
 		return decimal.NullDecimal{Valid: false}
 	}
-	return decimal.NullDecimal{Decimal: *d, Valid: true}
+	return decimal.NullDecimal{
+		Decimal: *d,
+		Valid:   true,
+	}
 }
 
 func DecimalPtr(d decimal.NullDecimal) *decimal.Decimal {
@@ -136,14 +224,24 @@ func DecimalPtr(d decimal.NullDecimal) *decimal.Decimal {
 }
 
 func NullBool(b bool) sql.NullBool {
-	return sql.NullBool{Bool: b, Valid: true}
+	return sql.NullBool{
+		Bool:  b,
+		Valid: true,
+	}
+}
+
+func NullBoolValid(b bool) sql.NullBool {
+	return NullBool(b)
 }
 
 func NullBoolPtr(b *bool) sql.NullBool {
 	if b == nil {
-		return sql.NullBool{Bool: false, Valid: false}
+		return sql.NullBool{Valid: false}
 	}
-	return sql.NullBool{Bool: *b, Valid: true}
+	return sql.NullBool{
+		Bool:  *b,
+		Valid: true,
+	}
 }
 
 func BoolPtr(b sql.NullBool) *bool {
@@ -154,14 +252,27 @@ func BoolPtr(b sql.NullBool) *bool {
 }
 
 func NullTime(t time.Time) sql.NullTime {
-	return sql.NullTime{Time: t, Valid: !t.IsZero()}
+	return sql.NullTime{
+		Time:  t,
+		Valid: !t.IsZero(),
+	}
+}
+
+func NullTimeValid(t time.Time) sql.NullTime {
+	return sql.NullTime{
+		Time:  t,
+		Valid: true,
+	}
 }
 
 func NullTimePtr(t *time.Time) sql.NullTime {
 	if t == nil {
-		return sql.NullTime{Time: time.Time{}, Valid: false}
+		return sql.NullTime{Valid: false}
 	}
-	return sql.NullTime{Time: *t, Valid: true}
+	return sql.NullTime{
+		Time:  *t,
+		Valid: true,
+	}
 }
 
 func TimePtr(t sql.NullTime) *time.Time {
